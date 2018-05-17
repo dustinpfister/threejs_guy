470f204d38d2e2732ee66e2bdfc8b406d9d747bc
dustinpfister <dustin.pfister@gmail.com>
2018-05-17 08:47:04 -0400

    three guys

--- main.js
+++ main.js
@@ -2,8 +2,16 @@
 var scene = new THREE.Scene();
 
 // GUY
-var guy = new Guy();
-scene.add(guy.group);
+var guy1 = new Guy();
+scene.add(guy1.group);
+
+var guy2 = new Guy();
+guy2.group.position.set(5, 0, 0);
+scene.add(guy2.group);
+
+var guy3 = new Guy();
+guy3.group.position.set(-5, 0, 0);
+scene.add(guy3.group);
 
 // CAMERA
 var camera = new THREE.PerspectiveCamera(50, 8 / 6, .05, 100);
@@ -23,12 +31,30 @@ var frame = 0, maxFrame = 100;
 var loop = function () {
 
     var per = frame / maxFrame,
-    bias = Math.abs(.5 - per) / .5;
+    bias = Math.abs(.5 - per) / .5,
+    r = Math.PI * 2 * per;
 
     requestAnimationFrame(loop);
 
-    guy.moveArm('arm_right', .4 + .2 * bias, .25);
-    guy.moveLegs(per);
+    // guy1 waves and walaks
+    guy1.moveArm('arm_right', .4 + .2 * bias, .25);
+    guy1.moveArm('arm_left',  - .05 + .1 * bias, 0);
+    guy1.moveHead(.05 - .1 * bias);
+    guy1.moveLegs(per * 8);
+    guy1.group.position.set(
+        Math.cos(r) * 5 - 5,
+        0,
+        Math.sin(r) * 5);
+    guy1.group.lookAt(
+        Math.cos(r + 1) * 5 - 5,
+        0,
+        Math.sin(r + 1) * 5);
+
+    // guy 2 shakes his head
+    guy2.moveHead(.125 - .25 * bias);
+
+    guy3.moveArm('arm_right', 0, bias * 2);
+    guy3.moveArm('arm_left', 0, bias * 2);
 
     renderer.render(scene, camera);
 
